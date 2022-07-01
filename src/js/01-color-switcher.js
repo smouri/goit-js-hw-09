3c5277d416992943a536efe00960e72311c503ca
Roman Babiy <romchik.babiy@gmail.com>
2022-07-01 10:15:19 +0300

    upd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,7 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
 
@@ -17,6 +21,3 @@ stopChangeBodyColor = () => {
     btnStart.disabled = false;
 };
 btnStop.addEventListener('click', stopChangeBodyColor);
-
-getRandomHexColor = () =>
-    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
