c930382d2e8430da2a0bb28d8b2ab3b7c7dd0ba8
smouri <97457197+smouri@users.noreply.github.com>
2022-07-01 06:21:07 +0000

    Deploying to gh-pages from @ smouri/goit-js-hw-09@65d97672c9ccdc63e0185e18a85e5e02b13481a9 🚀

---
+++ 01-color-switcher.758fb09f.js
@@ -0,0 +1,2 @@
+getRandomHexColor=()=>`#${Math.floor(16777215*Math.random()).toString(16)}`;const o=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let t=null;changeBodyColor=o=>{o.currentTarget.disabled=!0,t=setInterval((()=>{const o=getRandomHexColor();document.body.style.backgroundColor=o}),1e3)},o.addEventListener("click",changeBodyColor),stopChangeBodyColor=()=>{clearInterval(t),o.disabled=!1},e.addEventListener("click",stopChangeBodyColor);
+//# sourceMappingURL=01-color-switcher.758fb09f.js.map
